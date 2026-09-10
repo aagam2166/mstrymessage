38794a30224cafdcab9ff47da7761834c59974e2
Aagam2166 <aagam2166@gmail.com>
2026-09-10 14:28:17 +0530

    basic setup

---
+++ src/schemas/signUpSchema.ts
@@ -0,0 +1,9 @@
+import {z} from 'zod'
+
+export const usernameValidation = z.string().min(2,"username must be atleast 2 characters").max(20,"Username msut be no more than 20 characters").regex(/^[a-zA-Z0-9]+$/,"Username must not contain special character")
+
+export const signUpSchema = z.object({
+    username: usernameValidation,
+    email: z.email({message: 'Invalid email address'}),
+    password: z.string().min(6,{message: "Password must be atlesat 6 characters long"})
+})
